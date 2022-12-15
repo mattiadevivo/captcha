5a88c3af945101af147910c058dbcd3cac204608
Mattia De Vivo <mdevivo@came.com>
2022-12-15 10:15:03 +0100

    add missing test

--- test/captchaService.test.ts
+++ test/captchaService.test.ts
@@ -15,7 +15,45 @@ describe('captchaService', function () {
         assert.hasAllKeys(result, { id: '', captchaImage: '' });
     });
     it('validateCaptcha() should correctly validate captchas', async function () {
-        // TODO
+        const captachasToValidate = [
+            {
+                id: '1',
+                realValue: 'ABCDEF',
+                userValue: 'ABCDEF',
+                expectedResult: true,
+            },
+            {
+                id: '2',
+                realValue: 'AbcD3F',
+                userValue: 'Abcd3F',
+                expectedResult: false,
+            },
+            {
+                id: '3',
+                realValue: 'FghiJ',
+                userValue: 'FghiJ',
+                expectedResult: true,
+            },
+            {
+                id: '4',
+                realValue: 'FGH22',
+                userValue: 'Pino1',
+                expectedResult: false,
+            },
+        ];
+        for (const item of captachasToValidate) {
+            const isValid = await validateCaptcha(
+                (id: string): Promise<string | null> =>
+                    new Promise((resolve) => resolve(item.realValue)),
+                item.id,
+                item.userValue
+            );
+            assert.strictEqual(
+                isValid,
+                item.expectedResult,
+                `Expected ${isValid} to be ${item.expectedResult}`
+            );
+        }
     });
     it('validateCaptcha() should return false for unexisting captcha id', async function () {
         const isValid = await validateCaptcha(
